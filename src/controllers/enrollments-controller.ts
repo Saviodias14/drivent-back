e5b4fd2ab5cbd13f38fac176bb446b7e8ba7abdc
Savio Dias Viana <saviodiasviana@gmail.com>
2023-07-03 12:20:51 -0300

    Mudança na requisição do cep

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -31,8 +31,7 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
 
 // TODO - Receber o CEP do usuário por query params.
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
-  const cep = req.query.cep;
-  if( typeof cep!=='string') throw invalidDataError;
+  const cep = req.query.cep.toString()
   try {
     const address = await enrollmentsService.getAddressFromCEP(cep);
     res.status(httpStatus.OK).send(address);
